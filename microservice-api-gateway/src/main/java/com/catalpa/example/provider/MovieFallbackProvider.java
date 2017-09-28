8419bf48f1b2d1f5a4fa03fd126ab5e969603c03
wanchuan <165646633@qq.com>
2017-09-28 17:09:41 +0800

    upgrade to Dalston.SR3. add Zuul fallback.

---
+++ microservice-api-gateway/src/main/java/com/catalpa/example/provider/MovieFallbackProvider.java
@@ -0,0 +1,62 @@
+package com.catalpa.example.provider;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * Created by wanchuan on 2017/9/28.
+ */
+@Component
+public class MovieFallbackProvider implements ZuulFallbackProvider {
+    @Override
+    public String getRoute() {
+        return "microservice-consumer-movie-ribbon-with-hystrix";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return getStatusCode().value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return getStatusCode().getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("电影微服务不可用，请稍后再试。".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                MediaType mediaType = new MediaType("application", "json", Charset.forName("UTF-8"));
+                headers.setContentType(mediaType);
+                return headers;
+            }
+        };
+    }
+}
